688a4ecceb039b3de3f59f5097f85a82fe6bf640
Knyciek <55848043+Knyciek@users.noreply.github.com>
2019-12-10 12:25:27 +0100

    Permissions issues resolved

--- travelapp/MapPage.xaml.cs
+++ travelapp/MapPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
 
 namespace travelapp
@@ -10,6 +11,40 @@ namespace travelapp
         public MapPage()
         {
             InitializeComponent();
+
+            GetPermissions();
+
+        }
+
+        private async void GetPermissions()
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                    {
+                        await DisplayAlert("Need your location", "We need to access your location", "OK");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                    if (results.ContainsKey(Permission.LocationWhenInUse))
+                        status = results[Permission.LocationWhenInUse];
+                }
+                if (status == PermissionStatus.Granted)
+                {
+                    locationsMap.IsShowingUser = true;
+                }
+                else
+                {
+                    await DisplayAlert("Access not granted", "Sry dude, access not granted", "Cool, whateva");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
         }
     }
 }
